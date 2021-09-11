38f491d392dcbab076152f5b52e3336334440807
kamalesh <kamalsurea619@gmail.com>
2021-09-12 02:45:05 +0530

    created container deploy script

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,1 +1,26 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
+const domain = process.env.PRODUCTION_DOMAIN;
+// basically for the domain which we do not know at dev
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: `marketing@${domain}/marketing/remoteEntry.js`,
+        // here we are assuming that the file is hosted inside the marketing folder as same domain of all container child
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
--- packages/marketing/config/webpack.prod.js
+++ packages/marketing/config/webpack.prod.js
@@ -1,1 +1,23 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      exposes: {
+        "./Marketing": "./src/bootstrap",
+        // here we are assuming that the file is hosted inside the marketing folder as same domain of all container child
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
